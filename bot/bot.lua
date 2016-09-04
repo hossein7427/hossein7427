3d1af0cfa4bb95625d9edfbc0d4ffbccb6310ddb
hossein7427 <hosseinkhani1224@gmail.com>
2016-09-04 14:02:40 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,9 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
+    sudo_users = {7427
+      
+      },
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
